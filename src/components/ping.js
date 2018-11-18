f6f656bbe9954cafe9ca7afb08fb4c84ef177862
Alex Manning <alexandersmanning@gmail.com>
2018-11-18 12:12:34 -0800

    working version of importing sass styles

--- src/components/ping.js
+++ src/components/ping.js
@@ -1,13 +1,19 @@
 import RactiveBridge from '../bridge';
-import template from './ping-tpl.html';
-
 import { map } from 'rxjs/operators';
 
 import { ping } from '../actions/ping-actions'
 import store from '../store';
 
+import styles from '../styles/app.scss';
+
 const pingComponent = RactiveBridge.extend({
-  template: template,
+  template: `
+    <div>
+      <h2 class="${styles.alex__test}">Is Pinging</h2>
+      <div class="${styles.alex__test}">{{ isPinging }}</div>
+      <button class="${styles.alex__test}" on-click="clickAction">Click me!</button>
+    </div>
+  `,
   oninit() {
     this.set('isPinging', store.getState().ping.isPinging);
 
